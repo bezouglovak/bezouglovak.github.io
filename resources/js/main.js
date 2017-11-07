692b30953cf6f931ef4c2d453a15b0c4f436614f
Kristina B <bezouglovak@gmail.com>
2017-11-06 21:38:40 -0500

    First push. Index base done. Min styling. Some javascript added as well.

---
+++ resources/js/main.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+  $('.btn-nav').click(function() {
+    $('.topbar').toggleClass('active');
+  })
+})
+
+// $(document).ready(function() {
+//
+//   $('#ps-circle').circleProgress({
+//     value: 0.90,
+//     size: 80,
+//     lineCap: "round",
+//     emptyFill: "rgba(0, 0, 0, .1)",
+//     fill: {
+//       color: "white"
+//     }
+//   });
+//
+//   $('#il-circle').circleProgress({
+//     value: 0.80,
+//     size: 80,
+//     lineCap: "round",
+//     emptyFill: "rgba(0, 0, 0, .1)",
+//     fill: {
+//       color: "white"
+//     }
+//   });
+//
+//   $('#ae-circle').circleProgress({
+//     value: 0.70,
+//     size: 80,
+//     lineCap: "round",
+//     emptyFill: "rgba(0, 0, 0, .1)",
+//     fill: {
+//       color: "white"
+//     }
+//   });
+//
+//   $('#id-circle').circleProgress({
+//     value: 0.70,
+//     size: 80,
+//     lineCap: "round",
+//     emptyFill: "rgba(0, 0, 0, .1)",
+//     fill: {
+//       color: "white"
+//     }
+//   });
+//
+//   $('#hc-circle').circleProgress({
+//     value: 0.85,
+//     size: 80,
+//     lineCap: "round",
+//     emptyFill: "rgba(0, 0, 0, .1)",
+//     fill: {
+//       color: "white"
+//     }
+//   });
+//
+//   $('#ja-circle').circleProgress({
+//     value: 0.70,
+//     size: 80,
+//     lineCap: "round",
+//     emptyFill: "rgba(0, 0, 0, .1)",
+//     fill: {
+//       color: "white"
+//     }
+//   });
